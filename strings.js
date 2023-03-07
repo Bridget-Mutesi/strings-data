050841c0f62ab54d66f08ee163e5e6f01f580b95
Mutesi-Bridget <bridgettemutesi@gmail.com>
2023-03-07 18:38:20 +0300

    my first commit

---
+++ strings.js
@@ -0,0 +1,12 @@
+const word = "beautiful";
+const result = word.slice(0,-5,-6,-7,-8);
+console.log(result);
+
+String.prototype.insert = function(index,string){
+      if (index > 0)
+      {
+        return Food.substring(0,index) + food.substring()
+      }
+}
+
+
